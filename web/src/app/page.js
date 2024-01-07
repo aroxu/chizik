d7b6e53e1e73450e48e03cc13de851ee42cc9918
aroxu <30624945+aroxu@users.noreply.github.com>
2024-01-07 19:58:01 +0900

    :globe_with_meridians: Web 폴더 반영 모노레포

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,95 +1,90 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import "./page.scss";
+import Link from "next/link";
+
+const preventRightClick = (event) => {
+  event.preventDefault();
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="App">
+      <div className="App-container">
+        <div className="App-left">
+          <div className="App-content">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src="/logo.png"
+              width={0}
+              height={0}
+              sizes="25vmin"
+              className="App-logo"
+              alt="logo"
+              onContextMenu={preventRightClick}
             />
-          </a>
+            <h3>치지직 스트리머를 위한 디스코드 봇, "치직"</h3>
+
+            <a
+              href="https://discord.com/oauth2/authorize?client_id=1183664649418326079&scope=bot+applications.commands&permissions=149504"
+              role="button"
+              className="outline"
+            >
+              초대하기
+            </a>
+            <br />
+            <a href="https://discord.gg/3cr7mduVh4" role="button" className="contrast outline">
+              치직 봇 지원 서버
+            </a>
+            <br />
+            <p>
+              <a href="https://toss.me/aroxu" target="_blank" rel="noreferrer">
+                <span>개발자 도와주기</span>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="App-right">
+          <div className="App-content">
+            <Image
+              src="/screenshots/screenshot_1.png"
+              width={0}
+              height={0}
+              sizes="50vmin"
+              alt="디스코드 방송 알림"
+              className="Screenshot"
+              onContextMenu={preventRightClick}
+            />
+
+            <br />
+            <h2>디스코드에 방송 알림을 바로 보내보세요.</h2>
+            <p>나의 시청자들에게 쉽게 알림을 전송할 수 있습니다.</p>
+          </div>
+          <div className="App-content">
+            <Image
+              src="/screenshots/screenshot_2.png"
+              width={0}
+              height={0}
+              sizes="50vmin"
+              alt="방송 정보 조회"
+              className="Screenshot"
+              onContextMenu={preventRightClick}
+            />
+            <br />
+            <h2>채널 정보 조회하기</h2>
+            <p>누적 시청자 수, 현재 시청자 수 등 채널 정보를 쉽게 확인할 수 있습니다.</p>
+          </div>
+          <div className="App-content">
+            <h2>더 많은 기능을 추가할 예정입니다.</h2>
+            <p>미리 초대해두고 업데이트 될 기능을 발빠르게 만나보세요!</p>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className="App-footer">
+        <div>
+          <Link href="/policy/tos">이용약관</Link> | <Link href="/policy/privacy">개인정보 처리방침</Link>
+        </div>
+        <code>© {new Date().getFullYear()} aroxu. All rights reserved.</code>
+      </footer>
     </main>
-  )
+  );
 }
